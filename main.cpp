0ef6648bc20a9e059ea48f00dad5dad8e6b4c215
Lucho185 <168945029+Lucho185@users.noreply.github.com>
2024-05-05 08:09:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,58 +8,73 @@
 using namespace std;
 
 
-int main(){
+int main()
+{
 
 // Aquí deberíamos hacer los llamados a las funciones
 
     srand(time(NULL));
 
-    cout<<"GRAN CERDO" << endl;
-    cout<<"------------------"<<endl;
-    cout<<endl;
-    cout<< "1 - JUGAR" << endl;
-    cout<< "2 - ESTADISTICAS" << endl;
-    cout<< "3 - CERDITOS" << endl;
-    cout<< "0 - SALIR" << endl;
-    cout<<"------------------" << endl;
+    int opc=1;
 
-    cout<<"ANTES DE CONTINUAR POR FAVOR INGRESE UNA OPCION: " << endl;
-    cout<<"OPCION: ";
-    int opc;
-    cin>>opc;
-    system ("cls");
-    switch (opc) {
-    case 1:
+
+    while(opc!=0)
+    {
+
+        system ("cls");
+
+        cout<<"GRAN CERDO" << endl;
+        cout<<"------------------"<<endl;
+        cout<<endl;
+        cout<< "1 - JUGAR" << endl;
+        cout<< "2 - ESTADISTICAS" << endl;
+        cout<< "3 - CERDITOS" << endl;
+        cout<< "0 - SALIR" << endl;
+        cout<<"------------------" << endl;
+
+        cout<<"ANTES DE CONTINUAR POR FAVOR INGRESE UNA OPCION: " << endl;
+        cout<<"OPCION: ";
+
+        cin>>opc;
+
+        switch (opc)
+        {
+        case 1:
 
             ejecutarJuego();
 
+            break;
 
+        case 2:
+            cout<<"EL MAYOR PUNTAJE OBTENIDO CORRESPONDE A: ";
+            break;
 
-    break;
+        case 3:
+            cout<<"DESARROLADORES: " << endl;
+            cout<<"----------------------------------" << endl;
+            cout<<"ALUMNO: BIANCHI NICOLAS" << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: BALDOCCHI LUIS" << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: CATTANEO CARLOS" << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: ORLANDO AGUILERA" << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: RIOS EVER" << endl;
+            break;
 
-    case 2:
-        cout<<"EL MAYOR PUNTAJE OBTENIDO CORRESPONDE A: ";
-        break;
+        case 0:
+            cout<<"Ingreso un 0 como opcion, estamos saliendo del programa...";
+            break;
 
-    case 3:
-        cout<<"DESARROLADORES: " << endl;
-        cout<<"----------------------------------" << endl;
-        cout<<"ALUMNO: " << endl;
-        cout<<"::::::::::::::::::::::::::::::::::" << endl;
-        cout<<"ALUMNO: " << endl;
-        cout<<"::::::::::::::::::::::::::::::::::" << endl;
-        cout<<"ALUMNO: " << endl;
-        cout<<"::::::::::::::::::::::::::::::::::" << endl;
-        cout<<"ALUMNO: " << endl;
-        cout<<"::::::::::::::::::::::::::::::::::" << endl;
-        cout<<"ALUMNO: " << endl;
-    break;
+        default:
+            cout<<"No ingreso una opcion valida."<<endl;
+            break;
 
-    default:
-        cout<<"LA OPCION INGRESADA ES INCORRECTA. REINTENTE.";
+        }
+        system("pause");
+
     }
-
 
     return 0;
 }
-
