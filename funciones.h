8bd31500cd076c869aa252d7de7dc6c507a3b500
Rolo <84113089+rololevy@users.noreply.github.com>
2024-05-11 09:29:00 -0300

    Update funciones.h
    Se define encabezados e implentaciones de las funciones

--- funciones.h
+++ funciones.h
@@ -1,16 +1,84 @@
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <time.h>
 
-// Aquí tendremos las declaraciones/encabezados de la funciones
 
-int ejecutarJuego();
-int determinarComienzo(std::string j1, std::string j2);
-int determinarPuntajes(int mat_dados[2][3], int cant_dados, int posicion_jugador);
-int PrimerLanzDado();
-int dadoMasAlto(int jugador1_d1, int jugador1_d2, int jugador2_d1, int jugador2_d2);
+#include <iostream>
 
-#endif  // FUNCIONES_H_INCLUDED
 
+using namespace std;
+
+#include "ejecutar_juego.h"
+
+int main()
+{
+
+    while (true)
+    {
+        system("cls");
+
+        srand(time(NULL));
+
+
+        cout<<"GRAN CERDO" << endl;
+        cout<<"------------------"<<endl;
+        cout<<endl;
+        cout<< "1 - JUGAR" << endl;
+        cout<< "2 - ESTADISTICAS" << endl;
+        cout<< "3 - CERDITOS" << endl;
+        cout<< "0 - SALIR" << endl;
+        cout<<"------------------" << endl;
+
+        cout<<"ANTES DE CONTINUAR POR FAVOR INGRESE UNA OPCION: " << endl;
+        cout<<"OPCION: ";
+        int opc;
+        cin>>opc;
+        system ("cls");
+        switch (opc)
+        {
+        case 1:
+
+            ejecutarJuego();
+
+
+
+            break;
+
+        case 2:
+            cout<<"EL MAYOR PUNTAJE OBTENIDO CORRESPONDE A: ";
+            system("pause");
+            break;
+
+        case 3:
+            cout<<"DESARROLADORES: " << endl;
+            cout<<"----------------------------------" << endl;
+            cout<<"ALUMNO: " << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: " << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: " << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: " << endl;
+            cout<<"::::::::::::::::::::::::::::::::::" << endl;
+            cout<<"ALUMNO: " << endl;
+            system("pause");
+            break;
+
+        case 0:
+            cout<<"Gracias por jugar, Adios" << endl;
+            getchar();
+            return 0;
+            break;
+
+        default :
+            cout<<"LA OPCION INGRESADA ES INCORRECTA. REINTENTE."<<endl<<endl;
+            system("pause");
+            break;
+        }
+
+    }
+
+    return 0;
+}
 
