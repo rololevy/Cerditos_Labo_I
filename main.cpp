7b9d8f8363bdd500e0094a692652e95b4ddfaa59
Rolo <84113089+rololevy@users.noreply.github.com>
2024-06-10 09:16:10 +1100

    Update main.cpp
    Versión final pos entrega/defensa

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <time.h>
+#include <cstring>
+
 
 
 #include <iostream>
@@ -13,6 +15,11 @@ using namespace std;
 
 int main()
 {
+
+    string salida;
+    string nombreMax;
+    int puntajeMax;
+    bool huboJuego= false;
 
     while (true)
     {
@@ -35,46 +42,88 @@ int main()
         int opc;
         cin>>opc;
         system ("cls");
+
+        const string GREEN = "\033[32m";
+        const string PURPLE = "\033[35m";
+        const string WHITE = "\033[37m";
+
+        const string RESET = "\033[0m";
+        const string BOLD_PURPLE = "\033[1;35m";
+
+
         switch (opc)
         {
         case 1:
-
-            ejecutarJuego();
+            huboJuego = true;
+            ejecutarJuego(&nombreMax, &puntajeMax);
 
 
 
             break;
 
         case 2:
-            cout<<"EL MAYOR PUNTAJE OBTENIDO CORRESPONDE A: ";
+            if(huboJuego){
+            cout << BOLD_PURPLE;
+            cout << "****************************************************" << endl;
+            cout << "****************************************************" << endl;
+            cout << "**                                                **" << endl;
+            cout << "**            PUNTAJES FINALES                    **" << endl;
+            cout << "**                                                **" << endl;
+            cout << "****************************************************" << endl;
+            cout << "****************************************************" << endl;
+            cout << RESET << endl;
+
+            cout<<"NOMBRE DEL JUGADOR GANADOR: ";
+            cout<<nombreMax<<endl;
+            cout<<"PUNTOS DE VICTORIA: ";
+            cout<<puntajeMax<<endl;
+            cout<<endl;
+            cout<<WHITE;
+
             system("pause");
+            }
+            else {
+                cout << "No hay partidas recientes para mostrar puntaje."<<endl;
+                system("pause");
+                }
+
             break;
 
         case 3:
-            cout<<"DESARROLADORES: " << endl;
-            cout<<"----------------------------------" << endl;
-            cout<<"ALUMNO: " << endl;
-            cout<<"::::::::::::::::::::::::::::::::::" << endl;
-            cout<<"ALUMNO: " << endl;
-            cout<<"::::::::::::::::::::::::::::::::::" << endl;
-            cout<<"ALUMNO: " << endl;
-            cout<<"::::::::::::::::::::::::::::::::::" << endl;
-            cout<<"ALUMNO: " << endl;
-            cout<<"::::::::::::::::::::::::::::::::::" << endl;
-            cout<<"ALUMNO: " << endl;
+
+            cout <<"DESARROLADORES: " << endl;
+            cout << endl;
+            cout << "ALUMNO: " << GREEN << "Ever Benjamin Rios" << WHITE << endl;
+            cout << endl;
+            cout << "ALUMNO: " << GREEN << "Carlos Alberto Cattaneo"  << WHITE << endl;
+            cout << endl;
+            cout << "ALUMNO: " << GREEN << "Nicolás Emilio Bianchi" << WHITE << endl;
+            cout << endl;
+            cout << "ALUMNO: " << GREEN << "Orlando Aguilera"  << WHITE << endl;
+            cout << endl;
+            cout << "ALUMNO: " << GREEN << "Luis Baldocchi" << WHITE << endl;
+            cout <<endl;
+
             system("pause");
             break;
+
+
+
 
         case 0:
-            cout<<"Gracias por jugar, Adios" << endl;
-            getchar();
-            return 0;
+
+            cout<<" ¿DESEA SALIR DEL JUEGO?  (S/N) ";
+            cin>>salida;
+            if (salida == "s" || salida == "S")
+            {
+                return 0;
+            }
+
             break;
 
-        default :
-            cout<<"LA OPCION INGRESADA ES INCORRECTA. REINTENTE."<<endl<<endl;
-            system("pause");
-            break;
+
+        default:
+            cout<<"LA OPCION INGRESADA ES INCORRECTA. REINTENTE.";
         }
 
     }
